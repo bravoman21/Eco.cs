a8b045d602eb1917cf551d4fa254f68807da0b3e
bravoman21 <47053039+bravoman21@users.noreply.github.com>
2021-06-18 21:46:08 -0700

    Add files via upload

---
+++ Eco.cs/eco.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using WindowsGSM.Functions;
+using WindowsGSM.GameServer.Engine;
+using WindowsGSM.GameServer.Query;
+
+namespace WindowsGSM.Plugins
+{
+    public class Eco : SteamCMDAgent // SteamCMDAgent is used because Eco relies on SteamCMD for installation and update process
+    {
+        // - Plugin Details
+        public Plugin Plugin = new Plugin
+        {
+            name = "WindowsGSM.Eco", // WindowsGSM.XXXX
+            author = "BravoMan21",
+            description = "WindowsGSM plugin for supporting Eco Dedicated Server",
+            version = "1.01",
+            url = "https://github.com/bravoman21/WindowsGSM.Eco", // Github repository link (Best practice)
+            color = "#086203" // Color Hex
+        };
+
+
+        // - Standard Constructor and properties
+        public Eco(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
+        private readonly ServerConfig _serverData; // Store server start metadata, such as start ip, port, start param, etc
+
+
+        // - Settings properties for SteamCMD installer
+        public override bool loginAnonymous => false; // Eco requires to login steam account to install the server, so loginAnonymous = false
+        public override string AppId => "739590"; // Game server appId, Eco is 739590
+
+
+        // - Game server Fixed variables
+        public override string StartPath => "EcoServer.exe"; // Game server start path. For Eco it is called EcoServer.exe
+        public string FullName = "Eco Server"; // Game server FullName
+        public bool AllowsEmbedConsole = true;  // Does this server support output redirect?
+        public int PortIncrements = 2; // This tells WindowsGSM how many ports should skip after installation
+        public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
+
+
+        // - Game server default values
+        public string Port = "3000"; // Default port
+        public string CommunicationPort = "8766"; // Steam Communications Port
+        public string QueryPort = "3001"; // Default query port
+        public string Defaultmap = "empty"; // Default map name
+        public string Maxplayers = "-1"; // Default maxplayers
+        public string Additional = ""; // Additional server start parameter
+
+
+        // - Create a default cfg for the game server after installation
+        public async void CreateServerCFG() { }
+
+
+        // - Start server function, return its Process to WindowsGSM
+        public async Task<Process> Start()
+        {
+            // Prepare start parameter
+            var param = new StringBuilder();
+            param.Append(string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $" -port={_serverData.ServerPort}");
+            param.Append(string.IsNullOrWhiteSpace(_serverData.ServerName) ? string.Empty : $" -name=\"{_serverData.ServerName}\"");
+            param.Append(string.IsNullOrWhiteSpace(_serverData.ServerParam) ? string.Empty : $" {_serverData.ServerParam}");
+ 
+            // Prepare Process
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    WindowStyle = ProcessWindowStyle.Minimized,
+                    UseShellExecute = false,
+                    WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID),
+                    FileName = ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath),
+                    Arguments = param.ToString()
+                },
+                EnableRaisingEvents = true
+            };
+
+            // Start Process
+            try
+            {
+                p.Start();
+                return p;
+            }
+            catch (Exception e)
+            {
+                base.Error = e.Message;
+                return null; // return null if fail to start
+            }
+        }
+
+
+        // - Stop server function
+        public async Task Stop(Process p) => await Task.Run(() => { p.Kill(); });
+    }
+}
